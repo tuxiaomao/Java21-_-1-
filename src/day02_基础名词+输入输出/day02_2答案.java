b74ef50d6997cc5c8c3ec0359d828d0c4735a528
zhangyue <zhangyue1@lixiang.com>
2020-08-19 18:11:03 +0800

    java打卡第1期_day01

---
+++ src/day02_基础名词+输入输出/day02_2答案.java
@@ -0,0 +1,13 @@
+package day01_环境准备;
+
+
+public class day02_2答案 {
+    /**输入和输出
+     * 题目：
+     * 设计一个程序，输入上次考试成绩（int）和本次考试成绩（int），然后输出成绩提高的百分比，保留两位小数位。
+     * */
+    public static void main(String[] args){
+
+        System.out.println("HelloWorld");
+    }
+}
